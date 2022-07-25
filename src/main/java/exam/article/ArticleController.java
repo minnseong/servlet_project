34471cf6d7b50d512b9af5b35afdedc597aa6097
minseong <kdsvip5@naver.com>
2022-07-25 18:07:20 +0900

    간단한 게시글 작성폼 추가

--- src/main/java/exam/article/ArticleController.java
+++ src/main/java/exam/article/ArticleController.java
@@ -19,4 +19,8 @@ public class ArticleController {
         rq.setAttr("articles", articleDtos);
         rq.view("usr/article/list");
     }
+
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+    }
 }
